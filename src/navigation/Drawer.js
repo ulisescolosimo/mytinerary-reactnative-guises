54099398ada48442e46771dff7eec9ba1759cc29
GuidoSeia <guidoseia5@gmail.comguidoseia5@gmail.com>
2022-09-28 19:16:21 -0300

    agregado de navegacion al componente signUp

--- src/navigation/Drawer.js
+++ src/navigation/Drawer.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import HomeScreen from '../../Screens/HomeScreen'
 import Cities from '../../Screens/Cities'
+import SignUp from '../../Screens/SignUp'
 import { createDrawerNavigator } from '@react-navigation/drawer'
 import { NavigationContainer } from '@react-navigation/native';
 import Detail from '../../Screens/Detail';
@@ -17,9 +18,10 @@ return (
       width: 250,},}}>
       <DrawerNavigation.Screen name="Home" component={HomeScreen} />
       <DrawerNavigation.Screen name="Cities" component={Cities} />
-      <DrawerNavigation.Screen options={{
+      {/* <DrawerNavigation.Screen options={{
                   drawerItemStyle: { display: 'none' }
-        }} name="Details" component={Detail} />
+        }} name="Details" component={Detail} /> */}
+      <DrawerNavigation.Screen name="SignUp" component={SignUp} />
     </DrawerNavigation.Navigator>
 )
 }
