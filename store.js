69c5c77c50f3cb8b986a6b4d3dbd427cbb7d3797
ulisescolosimo <colosimo101@gmail.com>
2022-09-28 00:42:50 -0300

    Creamos store de redux

---
+++ store.js
@@ -0,0 +1,26 @@
+import { configureStore } from '@reduxjs/toolkit'
+import citiesApi from './src/features/citiesApi'
+import activityAPI  from './src/features/activitiesAPI'
+import itineraryAPI from './src/features/itineraryAPI'
+import myTineraryAPI from './src/features/myTineraryAPI'
+import usersAPI from './src/features/usersAPI'
+import loggedSlice from './src/features/loggedSlice'
+import commentsAPI from './src/features/commentsAPI'
+import refreshSlice from './src/features/refreshSlice'
+
+export default configureStore({
+  reducer: {
+    [citiesApi.reducerPath] : citiesApi.reducer,
+    [activityAPI.reducerPath]: activityAPI.reducer,
+    [itineraryAPI.reducerPath]: itineraryAPI.reducer,
+    [myTineraryAPI.reducerPath]: myTineraryAPI.reducer,
+    [usersAPI.reducerPath]: usersAPI.reducer,
+    [commentsAPI.reducerPath]: commentsAPI.reducer,
+    logged: loggedSlice,
+    refresh: refreshSlice
+  },
+  middleware: (getAllCities) => getAllCities({
+      immutableCheck: false,
+      serializableCheck: false
+  })
+})
