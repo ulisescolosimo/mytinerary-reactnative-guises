b681c35b557deb8ee4a515c37b0ecf16ff700222
ulisescolosimo <colosimo101@gmail.com>
2022-09-28 19:35:08 -0300

    Agregamos ruta de login y signup

--- src/navigation/Drawer.js
+++ src/navigation/Drawer.js
@@ -2,13 +2,26 @@ import React from 'react'
 import HomeScreen from '../../Screens/HomeScreen'
 import Cities from '../../Screens/Cities'
 import { createDrawerNavigator } from '@react-navigation/drawer'
-import { NavigationContainer } from '@react-navigation/native';
 import Detail from '../../Screens/Detail';
 import Activities from '../../Screens/Activities';
+import SignIn from '../../Screens/SignIn';
+import { useSelector, useDispatch } from 'react-redux';
+import { loggedTrue } from '../features/loggedSlice'
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import SignOut from '../../Screens/SignOut';
 
 const DrawerNavigation = createDrawerNavigator();
 
 export default function Drawer() {
+
+  const logged = useSelector((state) => state.logged.loggedState)
+  const dispatch = useDispatch()
+
+  const user = AsyncStorage.getItem('user')
+
+  if (user.length > 0) {
+    dispatch(loggedTrue())
+  }
 
 return (
   <DrawerNavigation.Navigator
@@ -18,6 +31,7 @@ return (
       width: 250,},}}>
       <DrawerNavigation.Screen name="Home" component={HomeScreen} />
       <DrawerNavigation.Screen name="Cities" component={Cities} />
+      { logged ? <DrawerNavigation.Screen name="Sign Out" component={SignOut} /> : <DrawerNavigation.Screen name="Sign In" component={SignIn} />}
       <DrawerNavigation.Screen options={{
                   drawerItemStyle: { display: 'none' }
         }} name="Details" component={Detail} />
