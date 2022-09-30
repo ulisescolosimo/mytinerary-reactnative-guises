0e6b53d34fbd18e70ce5cff36004f1798486de97
ulisescolosimo <colosimo101@gmail.com>
2022-09-30 12:06:29 -0300

    Last fixes

--- App.js
+++ App.js
@@ -1,40 +1,20 @@
-import React, { useCallback } from 'react'
-import { useEffect } from 'react'
+import React, { useEffect } from 'react'
+import { StatusBar } from  'react-native'
 import { NavigationContainer } from '@react-navigation/native'
 import Drawer from './src/navigation/Drawer' 
 import {Provider} from 'react-redux'
 import store from './store'
-import {useFonts} from 'expo-font'
-import { RobotoSlab_400Regular } from '@expo-google-fonts/dev'
-import * as SplashScreen from 'expo-splash-screen'
 
 export default function App() {
   
-  const [fontsLoaded] = useFonts({
-    RobotoSlab_400Regular,
-    Roboto: require('./assets/fonts/RobotoMono-Regular.ttf')
-  });
-
-
-  useEffect(() => {
-    async function prepare () {
-      await SplashScreen.preventAutoHideAsync();
-    }
-    prepare();
-  }, [])
-
-  const onLayout = useCallback(async () =>{
-    if(fontsLoaded) {
-      await SplashScreen.hideAsync();
-    }
-  }, [fontsLoaded])
-
-  if(!fontsLoaded) return null;
-
   return (
-    <Provider store={store} onLayout={onLayout}>
+    <Provider store={store}>
     <NavigationContainer>
       <Drawer />
+      <StatusBar 
+        backgroundColor="black"
+        barStyle= "light-content"
+      />
     </NavigationContainer>
     </Provider>
   )
